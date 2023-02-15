26d4412e823c917f16eb78c0c974fc7cda679e4d
rina <rina288@ewhain.net>
2023-02-15 17:17:39 +0900

    django course 38 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
 
