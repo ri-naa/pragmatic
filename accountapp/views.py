d354da5c414eb977e0751f4d70ca40924e7e635d
rina <rina288@ewhain.net>
2023-02-24 14:42:49 +0900

    django error modified

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,7 +21,7 @@ has_ownership= [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 
@@ -44,7 +44,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership, 'get')
